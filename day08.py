5552178b7b71b533024ba3c9faaae0f42c8f03dd
Glen Haber <GlenboLake@gmail.com>
2019-12-08 00:45:28 -0500

    Update Day 8 to use PIL

--- day08.py
+++ day08.py
@@ -1,17 +1,15 @@
-from pprint import pprint
+from PIL import Image
 
 width, height = 25, 6
 
 with open('input/day08.txt') as f:
     data = list(map(int, f.read().strip()))
 
-size = width*height
-
 layers = []
 while data:
     layer = []
-    for w in range(width):
-        for h in range(height):
+    for _ in range(width):
+        for _ in range(height):
             layer.append(data.pop(0))
     layers.append(layer)
 
@@ -19,17 +17,16 @@ zeros_layer = min(layers, key=lambda lay: lay.count(0))
 
 print(zeros_layer.count(1) * zeros_layer.count(2))
 
-BLACK = chr(9608)
-WHITE = chr(9617)
+img = Image.new(mode='1', size=(width, height))
 
 for row in range(height):
     for col in range(width):
         pixels = [layer.pop(0) for layer in layers]
         for x in pixels:
             if x == 0:
-                print(BLACK, end='')
+                img.putpixel((col, row), 0)
                 break
             elif x == 1:
-                print(WHITE, end='')
+                img.putpixel((col, row), 1)
                 break
-    print()
+img.show()
